412024c05d9b364118455ce1360cdee07bd4a8fa
timDeHof <this.is.tim.dehof@gmail.com>
2022-06-27 15:03:58 -0400

    added logic to update and display scores

--- index.js
+++ index.js
@@ -17,18 +17,19 @@ rollBtnEl.addEventListener('click', function () {
   const randomNumber = Math.floor(Math.random() * 6) + 1;
   console.log(randomNumber);
 
-  // 1. Display the dice number instead of logging it out
-  // 2. Use the 'active' class to display which players turn it is
-  // Hint: use the .classList.remove() method to remove and classList.add() method to add
-  // 3. Update the "message" DOM node so that it states who's turn it is
-  // Find out  which players turn it is & log out the value e.g "Player 1 rolled 5"
+  // 1. Update the scores for each player
+  // 2. Display the scores in their scoreboards
   if (player1Turn) {
     player1DiceEl.textContent = randomNumber;
+    player1Score += parseInt(randomNumber);
+    player1ScoreEl.textContent = player1Score;
     player1DiceEl.classList.remove('active');
     player2DiceEl.classList.add('active');
     messageEl.textContent = 'Player 2 Turn';
   } else {
     player2DiceEl.textContent = randomNumber;
+    player2Score += parseInt(randomNumber);
+    player2ScoreEl.textContent = player2Score;
     player2DiceEl.classList.remove('active');
     player1DiceEl.classList.add('active');
     messageEl.textContent = 'Player 1 Turn';
