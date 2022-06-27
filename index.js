73eb295433925b228d1f52d7b8bd1463b67bbe61
timDeHof <this.is.tim.dehof@gmail.com>
2022-06-27 14:19:56 -0400

    setting up the DOM nodes

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 let player1Score = 0;
 let player2Score = 0;
 let player1Turn = true;
+
+// Create variables to store references to the necessary DOM nodes
+const player1ScoreEl = document.getElementById('player1Scoreboard');
+const player2ScoreEl = document.getElementById('player2Scoreboard');
+const messageEl = document.getElementById('message');
+const player1DiceEl = document.getElementById('player1Dice');
+const player2DiceEl = document.getElementById('player2Dice');
+const rollBtnEl = document.getElementById('rollBtn');
+const resetBtnEl = document.getElementById('resetBtn');
